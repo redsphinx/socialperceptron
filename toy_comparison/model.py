210c3bf671e7d129971eeba6fe2cfb6bd348c746
redsphinx <gabi.ras@hotmail.com>
2018-08-06 16:24:06 +0200

    toy problem to only 1 weight and 1 bias

--- toy_comparison/model.py
+++ toy_comparison/model.py
@@ -9,30 +9,18 @@ class Branch(chainer.Chain):
         super(Branch, self).__init__()
         with self.init_scope():
             self.fc1 = Linear(in_size=1, out_size=1)
-            # self.bn1 = BatchNormalization(20)
-            # self.fc2 = Linear(in_size=20, out_size=1)
-            # self.bn2 = BatchNormalization(1)
 
     def __call__(self, x):
-        # h = self.fc1(x)
-        # h = self.bn1(h)
-        # h = relu(h)
-        # h = self.fc2(h)
-        # h = self.bn2(h)
         h = relu(x)
         return h
 
 
-class Siamese(chainer.Chain):
+class Siamese0(chainer.Chain):
     def __init__(self):
-        super(Siamese, self).__init__()
+        super(Siamese0, self).__init__()
         with self.init_scope():
             self.b1 = Branch()
-            # self.fc1 = Linear(in_size=1, out_size=1)
-            self.fc1 = Linear(in_size=2, out_size=2)
-            # self.fc2= Linear(in_size=1, out_size=2)
-            # self.fc3 = Linear(in_size=1, out_size=2)
-            # self.fc = Linear(in_size=1, out_size=2)
+            self.fc1 = Linear(in_size=2, out_size=1)
 
     def __call__(self, x1, x2):
         _1 = self.b1(x1)
@@ -42,9 +30,16 @@ class Siamese(chainer.Chain):
         h = _1 - _2
         h = self.fc1(h)
         h = relu(h)
-        # h = self.fc2(h)
-        # h = relu(h)
-        # h = self.fc3(h)
-        # h = softmax(h)
-        # print(h)
+        return h
+
+
+class Siamese(chainer.Chain):
+    def __init__(self):
+        super(Siamese, self).__init__()
+        with self.init_scope():
+            self.fc1 = Linear(in_size=1, out_size=1)
+
+    def __call__(self, x1, x2):
+        h = x1 - x2
+        h = self.fc1(h)
         return h
--- toy_comparison/train.py
+++ toy_comparison/train.py
@@ -5,6 +5,12 @@ from random import randint
 import chainer
 from chainer.functions import sigmoid_cross_entropy, mean_squared_error
 # from deepimpression2 import util as U
+import math
+
+
+def sigmoid(x):
+    s = 1 / (1 + math.exp(-x))
+    return s
 
 
 def binarize(arr):
@@ -14,10 +20,10 @@ def binarize(arr):
     new_arr = np.zeros(shapes, dtype=int)
 
     for j in range(shapes[0]):
-        if float(arr[j][0].data) > float(arr[j][1].data):
-            new_arr[j] = [1, 0]
+        if sigmoid(float(arr[j].data)) > 0.5:
+            new_arr[j] = 1
         else:
-            new_arr[j] = [0, 1]
+            new_arr[j] = 0
 
     return new_arr
 
@@ -30,13 +36,13 @@ def make_confusion_matrix(prediction, labels):
     tl, fl, tr, fr = 0, 0, 0, 0
 
     for i in range(shapes[0]):
-        if labels[i][0] == 1:
-            if prediction[i][0] == 1:
+        if labels[i] == 1:
+            if prediction[i] == 1:
                 tl += 1
             else:
                 fl += 1
-        elif labels[i][0] == 0:
-            if prediction[i][0] == 0:
+        elif labels[i] == 0:
+            if prediction[i] == 0:
                 tr += 1
             else:
                 fr += 1
@@ -46,68 +52,72 @@ def make_confusion_matrix(prediction, labels):
     return [tl, fl, tr, fr]
 
 
-model = Siamese()
-print(model.count_params())
-optimizer = Adam(alpha=0.0002, beta1=0.5, beta2=0.999, eps=10e-8)
-optimizer.setup(model)
+def main():
+    model = Siamese()
+    print('model params: ', model.count_params())
+    optimizer = Adam(alpha=0.0002, beta1=0.5, beta2=0.999, eps=10e-8)
+    optimizer.setup(model)
 
+    epochs = 1000
+    batch_size = 1
 
-epochs = 1000
-batch_size = 64
+    data_batch = np.zeros((batch_size, 2), dtype=np.float32)
 
-data_batch = np.zeros((batch_size, 2), dtype=np.float32)
+    labels = np.zeros((batch_size, 1), dtype=int)
 
-labels = np.zeros((batch_size, 2), dtype=int)
-# labels = np.zeros((batch_size, 2), dtype=np.float32)
+    loss_list = []
 
-loss_list = []
-
-for e in range(epochs):
-    for b in range(batch_size):
-        x1 = randint(0, 9)
-        x2 = randint(0, 9)
-        if x1 == x2:
-            lr = randint(0, 1) # decide which will be bigger
-            if x1 == 0:
-                if lr == 1: # left
-                    x1 += 1
+    for e in range(epochs):
+        for b in range(batch_size):
+            x1 = randint(0, 9)
+            x2 = randint(0, 9)
+            if x1 == x2:
+                lr = randint(0, 1) # decide which will be bigger
+                if x1 == 0:
+                    if lr == 1: # left
+                        x1 += 1
+                    else:
+                        x2 += 1
+                elif x1 == 9:
+                    if lr == 1:
+                        x2 -= 1
+                    else:
+                        x1 -= 1
                 else:
-                    x2 += 1
-            elif x1 == 9:
-                if lr == 1:
-                    x2 -= 1
-                else:
-                    x1 -= 1
-            else:
-                if lr == 1:
-                    x1 += 1
-                else:
-                    x2 += 1
+                    if lr == 1:
+                        x1 += 1
+                    else:
+                        x2 += 1
 
-        data_batch[b] = [np.float32(x1), x2]
-        # (1, 0) = left  (0, 1) = right
-        if x1 > x2: # left
-            # labels[b] = [np.float32(1), np.float32(0)]
-            labels[b] = [1, 0]
-        else: # right or equal
-            # labels[b] = [np.float32(0), np.float32(1)]
-            labels[b] = [0, 1]
+            data_batch[b] = [np.float32(x1), x2]
+            # (1, 0) = left  (0, 1) = right
+            if x1 > x2: # left
+                # labels[b] = [np.float32(1), np.float32(0)]
+                labels[b] = 1
+            else: # right or equal
+                # labels[b] = [np.float32(0), np.float32(1)]
+                labels[b] = 0
 
-    with chainer.using_config('train', True):
-        model.cleargrads()
-        d1 = np.expand_dims(data_batch[:, 0], -1)
-        d2 = np.expand_dims(data_batch[:, 1], -1)
-        # prediction = model(np.expand_dims(d1, 0), np.expand_dims(d2, 0))
-        prediction = model(d1, d2)
-        loss = sigmoid_cross_entropy(prediction, labels)
-        # loss = mean_squared_error(prediction, labels)
-        loss.backward()
-        optimizer.update()
+        with chainer.using_config('train', True):
+            model.cleargrads()
+            d1 = np.expand_dims(data_batch[:, 0], -1)
+            d2 = np.expand_dims(data_batch[:, 1], -1)
+            # prediction = model(np.expand_dims(d1, 0), np.expand_dims(d2, 0))
+            prediction = model(d1, d2)
+            loss = sigmoid_cross_entropy(prediction, labels)
+            # loss = mean_squared_error(prediction, labels)
+            loss.backward()
+            optimizer.update()
 
-    loss_list.append(float(loss.data))
+        loss_list.append(float(loss.data))
 
-    # print(e, float(loss.data))
+        # print(e, float(loss.data))
 
-    if (e+1) % 100 == 0:
-        cm = make_confusion_matrix(prediction, labels)
-        print(e, cm)
+        if (e+1) % 100 == 0:
+            cm = make_confusion_matrix(prediction, labels)
+            print(e, cm)
+
+
+for i in range(10):
+    print('iteration %d' % i)
+    main()
