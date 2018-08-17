4fedd0058de11df23edc6c4825bb2c95311ee605
redsphinx <gabi.ras@hotmail.com>
2018-08-17 16:55:00 +0200

    best todo ever

--- training_context_regression.py
+++ training_context_regression.py
@@ -1,3 +1,22 @@
 # TODO: for training with chalearn30, different background modes and regression individual traits
 # Q: what is the influence of background on personality prediction?
 
+'''
+TODO s
+
+- make data utils in order to get the data
+- make this file in same nature as sanity_check training
+- setup experiments like in my notes:
+__________________________________________
+|_____ test  |       |       |           |
+|    |_______| face  | BG    | BG + face |
+| train      |       |       |           |
+|------------|-------|-------|-----------|
+| face       |       |       |           |
+|------------|-------|-------|-----------|
+| BG         |       |       |           |
+|------------|-------|-------|-----------|
+| BG + face  |       |       |           |
+------------------------------------------
+
+'''
