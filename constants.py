f8d5d346321c43967735dd8548442ffad62abb60
redsphinx <gabi.ras@hotmail.com>
2019-03-31 13:55:58 +0200

    i dont know

--- constants.py
+++ constants.py
@@ -9,5 +9,7 @@ TRAIN_BATCH_SIZE = 32 # 32
 TEST_BATCH_SIZE = 1
 VAL_BATCH_SIZE = 32
 SEED = 6
+
+# for use in relative-baseline project
 OMG_EMPATHY_FRAME_RATE = 25
-SMOOTH_ANNOTATIONS_PATH = 'AnnotationsSmooth_75_3'
+SMOOTH_ANNOTATIONS_PATH= 'AnnotationsSmooth_75_3'
