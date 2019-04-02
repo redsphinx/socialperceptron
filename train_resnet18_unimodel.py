1c0e55c1f2abc0a1b589e2497a16fb8dfeab7309
redsphinx <gabi.ras@hotmail.com>
2019-04-03 01:46:12 +0200

    train_119 ready to train

--- train_resnet18_unimodel.py
+++ train_resnet18_unimodel.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 
 from torchvision.models import resnet18
 from torch.optim.adam import Adam
+from torch.optim.sgd import SGD
+from torch.optim import lr_scheduler
 import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss, L1Loss
@@ -21,9 +23,11 @@ import deepimpression2.chalearn30.data_utils as D
 
 
 # settings
+# TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+pretrained = True
+mode = 'finetune'  # or extractor
 num_traits = 5
-load_model = True
-
+load_model = False
 
 # device
 if C.ON_GPU:
@@ -32,41 +36,49 @@ else:
     _dev = 'cpu'
 device = torch.device(_dev)
 
+if mode == 'finetune':
+    # get model
+    my_model = resnet18(pretrained=pretrained)
+    my_model.fc = nn.Linear(in_features=512, out_features=num_traits, bias=True)
+    if load_model:
+        p = os.path.join(P.MODELS, '')
+        my_model.load_state_dict(torch.load(p))
+        print('model  loaded')
+        continuefrom = 0
+    else:
+        continuefrom = 0
+    my_model.cuda(device)
 
-# get model
-my_model = resnet18(pretrained=False)
-
-my_model.fc = nn.Linear(in_features=512, out_features=num_traits, bias=True)
-
-if load_model:
-    p = os.path.join(P.MODELS, 'epoch_19_107')
-    my_model.load_state_dict(torch.load(p))
-    print('model epoch_19_107 loaded')
-    continuefrom = 20
+    # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+elif mode == 'extractor':
+    my_model = resnet18(pretrained=pretrained)
+    for param in my_model.parameters():
+        param.requires_grad = False
+    my_model.fc = nn.Linear(in_features=512, out_features=num_traits, bias=True)
+    my_model = my_model.to(device)
 else:
-    continuefrom = 0
+    print('mode is not correct: %s' % mode)
+    my_model = None
 
-my_model.cuda(device)
-
-
-# Erdi tips: SGD, lr=0.001
-# https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-
-# optimizer
-learning_rate = 0.0002
-
-my_optimizer = Adam(
-            [
-                {'params': my_model.conv1.parameters(), 'lr': learning_rate/64},
-                {'params': my_model.bn1.parameters(), 'lr': learning_rate/32},
-                {'params': my_model.layer1.parameters(), 'lr': learning_rate/16},
-                {'params': my_model.layer2.parameters(), 'lr': learning_rate/8},
-                {'params': my_model.layer3.parameters(), 'lr': learning_rate/4},
-                {'params': my_model.layer4.parameters(), 'lr': learning_rate/2},
-                {'params': my_model.fc.parameters(), 'lr': learning_rate}
-            ],
-            lr=learning_rate, betas=(0.5, 0.999), eps=10-8
-        )
+learning_rate = 0.001
+momentum = 0.9
+my_optimizer = SGD(my_model.fc.parameters(), lr=learning_rate, momentum=momentum)
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(my_optimizer, step_size=7, gamma=0.1)
+    
+# learning_rate = 0.0002
+# my_optimizer = Adam(
+#             [
+#                 {'params': my_model.conv1.parameters(), 'lr': learning_rate/64},
+#                 {'params': my_model.bn1.parameters(), 'lr': learning_rate/32},
+#                 {'params': my_model.layer1.parameters(), 'lr': learning_rate/16},
+#                 {'params': my_model.layer2.parameters(), 'lr': learning_rate/8},
+#                 {'params': my_model.layer3.parameters(), 'lr': learning_rate/4},
+#                 {'params': my_model.layer4.parameters(), 'lr': learning_rate/2},
+#                 {'params': my_model.fc.parameters(), 'lr': learning_rate}
+#             ],
+#             lr=learning_rate, betas=(0.5, 0.999), eps=10-8
+#         )
 
 # loss
 loss_function = L1Loss()
@@ -79,23 +91,10 @@ epochs = C.EPOCHS
 # epochs = 1
 
 train_labels = h5.File(P.CHALEARN_TRAIN_LABELS_20, 'r')
-val_labels = h5.File(P.CHALEARN_VAL_LABELS_20, 'r')
-test_labels = h5.File(P.CHALEARN_TEST_LABELS_20, 'r')
-
 train_loss = []
 pred_diff_train = np.zeros((epochs, num_traits), float)
-
-val_loss = []
-pred_diff_val = np.zeros((epochs, num_traits), float)
-
-test_loss = []
-pred_diff_test = np.zeros((epochs, num_traits), float)
-
 training_steps = len(train_labels) // C.TRAIN_BATCH_SIZE
-val_steps = len(val_labels) // C.VAL_BATCH_SIZE
-test_steps = len(test_labels) // C.TEST_BATCH_SIZE
 # training_steps = 10
-# val_steps = 2
 
 id_frames = h5.File(P.NUM_FRAMES, 'r')
 
@@ -103,17 +102,13 @@ id_frames = h5.File(P.NUM_FRAMES, 'r')
 def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_saving, which_data, trait=None,
         ordered=False, save_all_results=False, record_predictions=False, record_loss=True, is_resnet18=True):
     print('steps: ', steps)
-    assert(which in ['train', 'test', 'val'])
+    assert(which in ['train'])
     assert(which_data in ['bg', 'face'])
     if trait is not None:
         assert(trait in ['O', 'C', 'E', 'A', 'S'])
 
     if which == 'train':
         which_batch_size = C.TRAIN_BATCH_SIZE
-    elif which == 'val':
-        which_batch_size = C.VAL_BATCH_SIZE
-    elif which == 'test':
-        which_batch_size = C.TEST_BATCH_SIZE
 
     loss_tmp = []
     pd_tmp = np.zeros((steps, num_traits), dtype=float)
@@ -126,43 +121,30 @@ def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_sa
 
     ts = time.time()
     for s in tqdm(range(steps)):
-        # HERE
-        # if which == 'test':
-        #     print(s)
-        # HERE
         labels_selected = _labs[s * which_batch_size:(s + 1) * which_batch_size]
         assert (len(labels_selected) == which_batch_size)
-        # labels, data, _ = D.load_data_single(labels_selected, which_labels, frames, which_data, resize=True,
-        #                                   ordered=ordered, trait=trait)
         labels, data, _ = D.load_data(labels_selected, which_labels, frames, which_data, ordered=ordered,
-                                      is_resnet18=is_resnet18)
+                                      is_resnet18=is_resnet18, resize=True)
 
         if C.ON_GPU:
             data = torch.from_numpy(data)
             data = data.cuda(device)
             labels = torch.from_numpy(labels)
-            # labels = labels.long()
             labels = labels.cuda(device)
 
-        if which == 'train':
-            model.train()
-            optimizer.zero_grad()
+        exp_lr_scheduler.step()
+        model.train()
+        optimizer.zero_grad()
+        with torch.set_grad_enabled(True):
             predictions = model(data)
             loss = loss_function(predictions, labels)
             loss.backward()
             optimizer.step()
             if bool(torch.isnan(loss)):
                 print('its happening')
-        else:
-            model.eval()
-            with torch.no_grad():
-                predictions = model(data)
-                loss = loss_function(predictions, labels)
-                loss = loss.detach()
 
         if record_loss:
             loss_tmp.append(float(loss.data))
-            # isn't this the same as MAE??
             pd_tmp[s] = U.pred_diff_trait(np.array(predictions.cpu().data), np.array(labels.cpu().data))
         if record_predictions and which == 'test':
             preds[s] = np.array(predictions.cpu().data)
@@ -185,46 +167,17 @@ def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_sa
 
 
 print('Enter training loop with validation')
-for e in range(continuefrom, epochs):
+for e in range(0, epochs):
     which_trait = None
-    train_on = 'bg'
-    validate_on = 'bg'
-    test_on = 'bg'
-    # print('trained on: %s test on %s for trait %s' % (train_on, test_on, which_trait))
+    train_on = 'face'
     # ----------------------------------------------------------------------------
     # training
     # ----------------------------------------------------------------------------
     run(which='train', steps=training_steps, which_labels=train_labels, frames=id_frames,
         model=my_model, optimizer=my_optimizer, pred_diff=pred_diff_train,
         loss_saving=train_loss, which_data=train_on, trait=which_trait)
-    # ----------------------------------------------------------------------------
-    # validation
-    # ----------------------------------------------------------------------------
-    # C2.ORDERED_FRAME = 6 # TODO SET THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # run(which='val', steps=val_steps, which_labels=val_labels, frames=id_frames,
-    #     model=my_model, optimizer=my_optimizer, pred_diff=pred_diff_val,
-    #     loss_saving=val_loss, which_data=validate_on, trait=which_trait, ordered=True)
-    # ----------------------------------------------------------------------------
-    # test
-    # ----------------------------------------------------------------------------
-    # C2.ORDERED_FRAME = 10 # TODO SET THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # times = 1
-    # for i in range(1):
-    #     if times == 1:
-    #         ordered = True
-    #         save_all_results = True
-    #     else:
-    #         ordered = False
-    #         save_all_results = False
-    # 
-    #     run(which='test', steps=test_steps, which_labels=test_labels, frames=id_frames,
-    #         model=my_model, optimizer=my_optimizer, pred_diff=pred_diff_test,
-    #         loss_saving=test_loss, which_data=test_on, ordered=ordered, save_all_results=save_all_results,
-    #         trait=which_trait, record_loss=False, record_predictions=True)
-    # best val 'bg': epoch_59_60_O, epoch_79_60_C, epoch_89_60_E, epoch_89_60_A, epoch_89_60_S
-    # best val 'face' OCEAS: epoch_39_59_O, epoch_49_59_C, epoch_99_59_E, epoch_89_59_A, epoch_19_59_S
 
     # save model
-    # if ((e + 1) % 10) == 0:
-    name = os.path.join(P.MODELS, 'epoch_%d_107' % e)
-    torch.save(my_model.state_dict(), name)
+    if ((e + 1) % 10) == 0:
+        name = os.path.join(P.MODELS, 'epoch_%d_119' % e)
+        torch.save(my_model.state_dict(), name)
