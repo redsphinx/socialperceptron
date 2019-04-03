fixed normalization issue

chalearn30/data_utils.py
@@ -14,7 +14,6 @@ import torchvision.transforms as transforms
 import torch
 
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 # commenting because of mxnet
 # def mp4_to_arr(video_path):
@@ -295,8 +294,20 @@ def fill_average(image, which_data, optface, resize=False):
                 return image
 
 
+def do_the_normalize(im):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    totensor = transforms.ToTensor()
+    im = im[0].astype(np.float32)
+    im = torch.from_numpy(im)
+    im = totensor(im)
+    im = normalize(im)
+    im = im.numpy()
+    im = np.expand_dims(im, 0)
+    return im
+
+
 def get_data(keys, id_frames, which_data, resize=False, ordered=False, twostream=False, frame_num=None,
-             use_luminance=False, use_color=False, use_everything=False, is_resnet18=False):
+             use_luminance=False, use_color=False, use_everything=False, is_resnet18=False, resnet18_pretrain=False):
     if resize:
         if twostream:
             data = np.zeros((len(keys), 6, C2.RESIDE, C2.RESIDE), dtype=np.float32)
@@ -322,6 +333,12 @@ def get_data(keys, id_frames, which_data, resize=False, ordered=False, twostream
                     bg = fill_average(bg, 'bg', optface, resize)
                     face, optface, n = quicker_load_resize(k, id_frames, 'face', ordered)
                     face = fill_average(face, 'face', optface, resize)
+                    # TODO: what is shape
+
+                    if is_resnet18 and resnet18_pretrain:
+                        face = do_the_normalize(face)
+                        bg = do_the_normalize(bg)
+
                     data[i] = np.concatenate((bg, face), axis=1)
         else:
             if frame_num is None:
@@ -330,12 +347,17 @@ def get_data(keys, id_frames, which_data, resize=False, ordered=False, twostream
                     image, optface, n = quicker_load_resize(k, id_frames, which_data, ordered)
                     frames.append(n)
                     image = fill_average(image, which_data, optface, resize)
+                    if is_resnet18 and resnet18_pretrain:
+                        image = do_the_normalize(image)
+
                     data[i] = image
                 n = frames
             else:
                 for i, k in enumerate(keys):
                     image, optface, n = quicker_load_resize(k, id_frames, which_data, ordered, frame_num[i])
                     image = fill_average(image, which_data, optface, resize)
+                    if is_resnet18 and resnet18_pretrain:
+                        image = do_the_normalize(image)
                     data[i] = image
                 n = frame_num
 
@@ -350,30 +372,19 @@ def get_data(keys, id_frames, which_data, resize=False, ordered=False, twostream
                 data[i] = get_color(image)
             elif use_everything:
                 data[i] = get_everything(image)
-            elif is_resnet18:
-                # normalize is torch resnet18
-                img_max = np.max(image)
-                if img_max == 0:
-                    data[i] = image
-                else:
-                    image = image[0].astype(np.float32)
-                    # image /= img_max
-                    for ch in range(3):
-                        if np.max(image[ch]) != 0:
-                            image[ch] = image[ch] / np.max(image[ch])
-                    image = torch.from_numpy(image)
-                    image = normalize(image)
-                    image = image.numpy()
-                    image = np.expand_dims(image, 0)
-                    data[i] = image
+            # ONLY normalize if using pretrained resnet18
+            elif is_resnet18 and resnet18_pretrain:
+                image = do_the_normalize(image)
+                data[i] = image
             else:
                 data[i] = image
+
 
     return data, n
 
 
 def load_data(labs_selected, labs_h5, id_frames, which_data, resize=False, ordered=False, twostream=False,
-              frame_num=None, same_frame=False, is_resnet18=False):
+              frame_num=None, same_frame=False, is_resnet18=False, resnet18_pretrain=False):
     assert(which_data in ['bg', 'face', 'all'])
 
     labels = np.zeros((len(labs_selected), 5), dtype=np.float32)
@@ -390,7 +401,8 @@ def load_data(labs_selected, labs_h5, id_frames, which_data, resize=False, order
         keys.append(k)
         labels[i] = labs_h5[k][0:5]
 
-    data, n = get_data(keys, id_frames, which_data, resize, ordered, twostream, frame_num, is_resnet18=is_resnet18)
+    data, n = get_data(keys, id_frames, which_data, resize, ordered, twostream, frame_num, is_resnet18=is_resnet18,
+                       resnet18_pretrain=resnet18_pretrain)
     return labels, data, n
 
 

train_resnet18_bimodal.py
@@ -21,7 +21,7 @@ import deepimpression2.util as U
 import deepimpression2.chalearn30.data_utils as D
 from deepimpression2.model_resnet18 import ResNet18_LastLayers
 
-
+PRETRAIN = True
 num_traits = 5
 # mode in ['finetune'] or ['extractor']
 mode = 'extractor' 
@@ -47,12 +47,12 @@ if mode == 'finetune':
     print('bg model epoch_99_102 loaded')
     
 elif mode == 'extractor':
-    face_model = resnet18(pretrained=True)
+    face_model = resnet18(pretrained=PRETRAIN)
     for param in face_model.parameters():
         param.requires_grad = False
     face_model.fc = nn.Sequential()
 
-    bg_model = resnet18(pretrained=True)
+    bg_model = resnet18(pretrained=PRETRAIN)
     for param in bg_model.parameters():
         param.requires_grad = False
     bg_model.fc = nn.Sequential()
@@ -105,7 +105,8 @@ id_frames = h5.File(P.NUM_FRAMES, 'r')
 
 
 def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_saving, which_data, trait=None,
-        ordered=False, save_all_results=False, record_predictions=False, record_loss=True, is_resnet18=True):
+        ordered=False, save_all_results=False, record_predictions=False, record_loss=True, is_resnet18=True,
+        pretrain_resnet=PRETRAIN):
     print('steps: ', steps)
     assert(which in ['train'])
     assert(which_data in ['all'])
@@ -130,10 +131,10 @@ def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_sa
         # keep resize=False. control with the case NO pre-trained resnet18
         labels_bg, bg_data, frame_num = D.load_data(labels_selected, which_labels, frames, which_data='bg',
                                                     ordered=False, is_resnet18=is_resnet18, same_frame=False,
-                                                    resize=True)
+                                                    resize=True, resnet18_pretrain=pretrain_resnet)
         labels_face, face_data, _ = D.load_data(labels_selected, which_labels, frames, which_data='face',
                                                 ordered=False, is_resnet18=is_resnet18, same_frame=True,
-                                                frame_num=frame_num, resize=True)
+                                                frame_num=frame_num, resize=True, resnet18_pretrain=pretrain_resnet)
 
         if C.ON_GPU:
             bg_data = torch.from_numpy(bg_data)

train_resnet18_unimodel.py
@@ -24,7 +24,7 @@ import deepimpression2.chalearn30.data_utils as D
 
 # settings
 # TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-pretrained = True
+PRETRAIN = True
 mode = 'finetune'  # or extractor
 num_traits = 5
 load_model = False
@@ -38,7 +38,7 @@ device = torch.device(_dev)
 
 if mode == 'finetune':
     # get model
-    my_model = resnet18(pretrained=pretrained)
+    my_model = resnet18(pretrained=PRETRAIN)
     my_model.fc = nn.Linear(in_features=512, out_features=num_traits, bias=True)
     if load_model:
         p = os.path.join(P.MODELS, '')
@@ -51,7 +51,7 @@ if mode == 'finetune':
 
     # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
 elif mode == 'extractor':
-    my_model = resnet18(pretrained=pretrained)
+    my_model = resnet18(pretrained=PRETRAIN)
     for param in my_model.parameters():
         param.requires_grad = False
     my_model.fc = nn.Linear(in_features=512, out_features=num_traits, bias=True)
@@ -107,7 +107,8 @@ id_frames = h5.File(P.NUM_FRAMES, 'r')
 
 
 def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_saving, which_data, trait=None,
-        ordered=False, save_all_results=False, record_predictions=False, record_loss=True, is_resnet18=True):
+        ordered=False, save_all_results=False, record_predictions=False, record_loss=True, is_resnet18=True,
+        pretrain_resnet=PRETRAIN):
     print('steps: ', steps)
     assert(which in ['train'])
     assert(which_data in ['bg', 'face'])
@@ -131,7 +132,7 @@ def run(which, steps, which_labels, frames, model, optimizer, pred_diff, loss_sa
         labels_selected = _labs[s * which_batch_size:(s + 1) * which_batch_size]
         assert (len(labels_selected) == which_batch_size)
         labels, data, _ = D.load_data(labels_selected, which_labels, frames, which_data, ordered=ordered,
-                                      is_resnet18=is_resnet18, resize=True)
+                                      is_resnet18=is_resnet18, resize=True, resnet18_pretrain=pretrain_resnet)
 
         if C.ON_GPU:
             data = torch.from_numpy(data)

validation_test_only_resnet_unimodal.py
@@ -15,7 +15,7 @@ from torch import nn
 from torch.nn import L1Loss
 
 
-def initialize(which, model_name):
+def initialize(which, model_name, pretrain):
     num_traits = 5
     load_model = True
 
@@ -25,7 +25,7 @@ def initialize(which, model_name):
         _dev = 'cpu'
     device = torch.device(_dev)
 
-    my_model = resnet18(pretrained=False)
+    my_model = resnet18(pretrained=pretrain)
     my_model.fc = nn.Linear(in_features=512, out_features=num_traits, bias=True)
 
     if load_model:
@@ -60,7 +60,8 @@ def initialize(which, model_name):
 
 
 def run(which, steps, which_labels, frames, model, pred_diff, loss_saving, which_data, trait, ordered,
-        save_all_results, record_predictions, record_loss, is_resnet18, num_traits, device, loss_function):
+        save_all_results, record_predictions, record_loss, is_resnet18, num_traits, device, loss_function,
+        resnet18_pretrain):
     print('steps: ', steps)
     assert (which in ['test', 'val'])
     assert (which_data in ['bg', 'face'])
@@ -83,7 +84,7 @@ def run(which, steps, which_labels, frames, model, pred_diff, loss_saving, which
         labels_selected = _labs[s * which_batch_size:(s + 1) * which_batch_size]
         assert (len(labels_selected) == which_batch_size)
         labels, data, _ = D.load_data(labels_selected, which_labels, frames, which_data, ordered=ordered,
-                                      is_resnet18=is_resnet18)
+                                      is_resnet18=is_resnet18, resnet18_pretrain=resnet18_pretrain)
 
         if C.ON_GPU:
             data = torch.from_numpy(data)
@@ -122,6 +123,7 @@ def run(which, steps, which_labels, frames, model, pred_diff, loss_saving, which
 
 def main_loop(which, val_test_on):
     which_trait = None
+    PRETRAIN = False
 
     if val_test_on == 'face':
         model_number = 101
@@ -139,19 +141,19 @@ def main_loop(which, val_test_on):
 
     for i, model_name in enumerate(models_to_load):
         my_model, labels, steps, loss, pred_diff, id_frames, loss_function, device, num_traits = \
-            initialize(which, model_name)
+            initialize(which, model_name, pretrain=PRETRAIN)
 
         if which == 'val':
             run(which=which, steps=steps, which_labels=labels, frames=id_frames, model=my_model, pred_diff=pred_diff,
                 loss_saving=loss, which_data=val_test_on, trait=which_trait, ordered=True,
                 save_all_results=False, record_predictions=False, record_loss=True, is_resnet18=True,
-                num_traits=num_traits, device=device, loss_function=loss_function)
+                num_traits=num_traits, device=device, loss_function=loss_function, resnet18_pretrain=PRETRAIN)
 
         elif which == 'test':
             run(which=which, steps=steps, which_labels=labels, frames=id_frames, model=my_model, pred_diff=pred_diff,
                 loss_saving=loss, which_data=val_test_on, trait=which_trait, ordered=True,
                 save_all_results=True, record_predictions=True, record_loss=True, is_resnet18=True,
-                num_traits=num_traits, device=device, loss_function=loss_function)
+                num_traits=num_traits, device=device, loss_function=loss_function, resnet18_pretrain=PRETRAIN)
 
 
 main_loop('test', 'bg')